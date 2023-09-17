9c777b205bd7febc1693d23a96853f6cde1bea6b
anonimus007-700 <sahsafg80@gmail.com>
2023-09-17 18:27:02 +0300

    1 ult

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import random
 import pyautogui
 import threading
 
+from pynput.mouse import Listener
+
 from PyQt5.QtWidgets import * 
 from PyQt5.QtGui import * 
 from PyQt5.QtCore import *
@@ -16,12 +18,15 @@ class Window(QMainWindow):
         self.setWindowTitle("Python ")
         
         self.setGeometry(QDesktopWidget().availableGeometry())
+        self.setMouseTracking(True)
 
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint) # Qt.FramelessWindowHint | 
         self.setAttribute(Qt.WA_TranslucentBackground)
 
         self.speed = 15
+        self.jail_work = False
         self.cords = []
+        self.jail_objects = []
 
         self.defoult_skin = QPixmap('../res/sania.png')
         self.sania_ult_1_skin = QPixmap('../res/sania_ult_1.png')
@@ -31,7 +36,11 @@ class Window(QMainWindow):
 
         self.show()
         
-        self.jail_event = threading.Thread(target=self.mouseMoveEvent)
+        self.mouse()
+        # self.mous = threading.Thread(target=self.mouse)
+        # self.jail_thread = threading.Thread(target=self.jailMoveEvent)
+        
+        # self.mous.start()
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.move_player)
@@ -40,6 +49,14 @@ class Window(QMainWindow):
         self.ult_timer = QTimer(self)
         self.ult_timer.timeout.connect(self.sania_ult_1)
         self.ult_timer.start(random.randint(5000, 15000))
+        
+    def thread(func):
+        def wrapper(*args, **kwargs):
+            current_thread = threading.Thread(
+                target=func, args=args, kwargs=kwargs)
+            current_thread.start()
+
+        return wrapper
 
     def UiComponents(self):
         self.player = QLabel(self)
@@ -89,30 +106,30 @@ class Window(QMainWindow):
         self.timer.stop()
         self.ult_timer.stop()
 
-        self.jail_event.start()
-
         self.player.setPixmap(self.sania_ult_1_skin)
 
-        jail_x = [self.curentx - 40, self.curentx + 40]
-        jail_y = [self.curenty - 40, self.curenty + 40]
+        jail_x = [self.current_x - 40, self.current_x + 40]
+        jail_y = [self.current_y - 40, self.current_y + 40]
         
         for i in jail_x:
             self.wall = QLabel(self)
             self.wall.setGeometry(i,
-                             self.y-30,
+                             self.current_y-30,
                              20,
                              80)
             self.wall.setStyleSheet('''background-color: black;
                                 border:3px solid white;
                                 border-radius: 10px;''')
             self.layout().addWidget(self.wall)
+            
+            self.jail_objects.append(self.wall)
 
             self.cords.append(self.wall.x())
             self.cords.append(self.wall.y())
 
         for i in jail_y:
             self.wall = QLabel(self)
-            self.wall.setGeometry(self.x-30,
+            self.wall.setGeometry(self.current_x-30,
                              i,
                              80,
                              20)
@@ -120,43 +137,55 @@ class Window(QMainWindow):
                                 border:3px solid white;
                                 border-radius: 10px;''')
             self.layout().addWidget(self.wall)
+            
+            self.jail_objects.append(self.wall)
 
             self.cords.append(self.wall.x())
             self.cords.append(self.wall.y())
 
         print(self.cords)
 
-        self.x_limit = (self.cords[0], self.cords[4])  # Приклад обмежень по осі X
-        self.y_limit = (self.cords[1], self.cords[5])  # Приклад обмежень по осі Y
+        self.x_limit = (self.cords[0], self.cords[2])
+        self.y_limit = (self.cords[1], self.cords[7])
 
-        self.jail_timer = QTimer(self)
-        self.jail_timer.timeout.connect(self.jailMoveEvent)
-        self.jail_timer.start(4000)
-        
-        self.jail_event.join()
+        self.jail_work = True
+
+        self.jailMoveEvent()
 
         self.continue_timer = QTimer(self)
         self.continue_timer.timeout.connect(self._continue)
         self.continue_timer.start(5000)
+    
+    @thread
+    def mouse(self):
+        with Listener(on_move=self.on_move) as listener:
+            listener.join()
 
+    def on_move(self, x, y):
+        self.current_x = x
+        self.current_y = y
+
+    @thread
     def jailMoveEvent(self):
-        while True:
-            self.curentx, self.curenty = pyautogui.position()
-            
-            if self.curentx < self.x_limit[0] or self.curentx > self.x_limit[1] or self.curenty < self.y_limit[0] or self.curenty > self.y_limit[1]:
+        while self.jail_work:
+            if self.current_x < self.x_limit[0] or self.current_x > self.x_limit[1] or self.current_y < self.y_limit[0] or self.current_y > self.y_limit[1]:
                 # Перемістіть курсор миші в середину визначених обмежень
-                new_x = max(self.x_limit[0], min(self.curentx, self.x_limit[1]))
-                new_y = max(self.y_limit[0], min(self.curenty, self.y_limit[1]))
+                new_x = max(self.x_limit[0], min(self.current_x, self.x_limit[1]))
+                new_y = max(self.y_limit[0], min(self.current_y, self.y_limit[1]))
                 pyautogui.moveTo(new_x, new_y)
 
     def _continue(self):
         self.timer.start(random.randint(1200, 4000))
         self.ult_timer.start(random.randint(5000, 15000))
-        
+
         self.continue_timer.stop()
+
+        for obj in self.jail_objects:
+            obj.deleteLater()
         
         self.cords.clear()
-        self.setMouseTracking(False)
+        self.jail_objects.clear()
+        self.jail_work = False
 
         self.player.setPixmap(self.defoult_skin)
 
