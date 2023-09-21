fe4923e618a949b4c5ccd431bd70aaa81183c59f
anonimus007-700 <sahsafg80@gmail.com>
2023-09-21 12:17:34 +0300

    add 2 player

--- src/main.py
+++ src/main.py
@@ -23,8 +23,10 @@ class Window(QMainWindow):
         
         self.setGeometry(QDesktopWidget().availableGeometry())
         self.setMouseTracking(True)
+        
+        self.window_flags = Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint
 
-        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint) # Qt.FramelessWindowHint | 
+        self.setWindowFlags(self.window_flags)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
         self.speed = 15
@@ -42,8 +44,20 @@ class Window(QMainWindow):
         self.stas_ult_1_skin = QPixmap('../res/stas/stas_ult_1.png')
         self.stas_ult_3_skin = QPixmap('../res/stas/stas_ult_3.png')
         
+        self.misha_defoult_skin = QPixmap('../res/misha/misha.png')
+        self.misha_ult_1_skin = QPixmap('../res/misha/misha_ult_1.png')
+        self.misha_ult_2_skin = QPixmap('../res/misha/misha_ult_2.png')
+        
+        self.kolia_defoult_skin = QPixmap('../res/kolia/kolia.png')
+        self.kolia_ult_1_skin = QPixmap('../res/kolia/kolia_ult_1.png')
+        
         self.stas_vensday = QSoundEffect()
         self.stas_vensday.setSource(QUrl.fromLocalFile("../res/stas/audio/vensday.wav"))
+        
+        self.misha_sigma = QSoundEffect()
+        self.misha_sigma.setSource(QUrl.fromLocalFile("../res/misha/audio/sigma.wav"))
+        self.misha_dance = QSoundEffect()
+        self.misha_dance.setSource(QUrl.fromLocalFile("../res/misha/audio/dance.wav"))
         
         self.sania_svarka = QSoundEffect()
         self.sania_svarka.setSource(QUrl.fromLocalFile("../res/sania/audio/svarka.wav"))
@@ -63,13 +77,19 @@ class Window(QMainWindow):
         
         self.sania_random_choice = random.choice([self.sania_ult_1, self.sania_ult_2])
         self.stas_random_choice = random.choice([self.stas_ult_1, self.stas_ult_2, self.stas_ult_3])
+        self.misha_random_choice = random.choice([self.misha_ult_1, self.misha_ult_2])
 
         self.ult_timer = QTimer(self)
         if self.player_chose == 'sania':
             self.ult_timer.timeout.connect(self.sania_random_choice)
-        else:
+        elif self.player_chose == 'stas':
             self.ult_timer.timeout.connect(self.stas_random_choice)
-        self.ult_timer.start(random.randint(8000, 30000))
+        elif self.player_chose == 'misha':
+            self.ult_timer.timeout.connect(self.misha_random_choice)
+        elif self.player_chose == 'kolia':
+            pass
+
+        self.ult_timer.start(random.randint(8000, 10000))
 
     def thread(func):
         def wrapper(*args, **kwargs):
@@ -113,9 +133,31 @@ class Window(QMainWindow):
                                 font-weight: bold;
                                 """)
         self.stas_but.clicked.connect(lambda: self._chose_player('stas'))
+        
+        self.misha_but = QPushButton("Міша", self)
+        self.misha_but.setIcon(QIcon(self.misha_defoult_skin))
+        self.misha_but.setIconSize(self.misha_defoult_skin.size())
+        self.misha_but.setStyleSheet("""
+                                color: white;
+                                background-color: transparent;
+                                font-weight: bold;
+                                """)
+        self.misha_but.clicked.connect(lambda: self._chose_player('misha'))
+        
+        self.kolia_but = QPushButton("Коля", self)
+        self.kolia_but.setIcon(QIcon(self.kolia_defoult_skin))
+        self.kolia_but.setIconSize(self.kolia_defoult_skin.size())
+        self.kolia_but.setStyleSheet("""
+                                color: white;
+                                background-color: transparent;
+                                font-weight: bold;
+                                """)
+        self.kolia_but.clicked.connect(lambda: self._chose_player('kolia'))
 
         self.button_chose.addWidget(self.sania_but)
+        self.button_chose.addWidget(self.kolia_but)
         self.button_chose.addWidget(self.stas_but)
+        self.button_chose.addWidget(self.misha_but)
 
         self.button_chose.setAlignment(Qt.AlignCenter)
 
@@ -140,6 +182,7 @@ class Window(QMainWindow):
         self.animation.start()
         
         self.timer.start(random.randint(1200, 4000))
+
 
     def sania_ult_1(self):
         self.timer.stop()
@@ -251,11 +294,10 @@ class Window(QMainWindow):
         self.sleeping.setGraphicsEffect(self.opacity_effect)
         
         self.sleep_anim = QPropertyAnimation(self.opacity_effect, b"opacity")
-        self.sleep_anim.setDuration(2000)  # Тривалість анімації (2 секунди)
-        self.sleep_anim.setStartValue(0.0)  # Початкова прозорість (0)
-        self.sleep_anim.setEndValue(0.9)  # Кінцева прозорість (0.9)
-        self.sleep_anim.setEasingCurve(QEasingCurve.OutQuad)  # Крива ефекту
-
+        self.sleep_anim.setDuration(2000)
+        self.sleep_anim.setStartValue(0.0)
+        self.sleep_anim.setEndValue(0.9)
+        self.sleep_anim.setEasingCurve(QEasingCurve.OutQuad)
 
         self.layout().addWidget(self.sleeping)
         self.player.raise_()
@@ -265,7 +307,7 @@ class Window(QMainWindow):
         self.sleeping_text.setGeometry(int(self.width()/2), int(self.height()/2), 200, 50)
         
         font = QFont()
-        font.setPointSize(16)  # Задаємо розмір шрифту 16
+        font.setPointSize(16)
         self.sleeping_text.setFont(font)
         
         self.layout().addWidget(self.sleeping_text)
@@ -274,7 +316,7 @@ class Window(QMainWindow):
         
         self.continue_timer = QTimer(self)
         self.continue_timer.timeout.connect(self._continue_stas)
-        self.continue_timer.start(6000)
+        self.continue_timer.start(7000)
 
     def stas_ult_2(self):
         self.timer.stop()
@@ -308,6 +350,61 @@ class Window(QMainWindow):
         self.continue_timer.timeout.connect(self._continue_stas)
         self.continue_timer.start(19000)
 
+    def misha_ult_1(self):
+        self.timer.stop()
+        self.ult_timer.stop()
+        
+        self.player.setPixmap(self.misha_ult_1_skin)
+        
+        self.misha_sigma.play()
+        
+        self.continue_timer = QTimer(self)
+        self.continue_timer.timeout.connect(self._continue_misha)
+        self.continue_timer.start(20500)
+    
+    def misha_ult_2(self):
+        self.timer.stop()
+        self.ult_timer.stop()
+
+        self.in_work = True
+        
+        self.dance = QLabel(self)
+        self.dance.setGeometry(0, 0, self.width(), self.height())
+        self.dance.setStyleSheet('background-color: black;')
+        
+        self.opacity_effect = QGraphicsOpacityEffect()
+        self.opacity_effect.setOpacity(0.5)
+        self.dance.setGraphicsEffect(self.opacity_effect)
+        
+        self.window = QLabel(self)
+        self.window.setGeometry(self.current_x,
+                                self.current_x,
+                                self.width(),
+                                self.height())
+
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.WindowTransparentForInput)
+        self.show()
+
+        self.layout().addWidget(self.dance)
+        self.layout().addWidget(self.window)
+        self.player.raise_()
+
+        self.misha_dance.play()
+
+        self.player.setPixmap(self.misha_ult_2_skin)
+
+        self.color_index = 0
+        self.colors = [QColor("red"), QColor("green"), QColor("blue"), QColor("yellow"), QColor("white"), QColor("pink")]
+
+        self.dance_timer = QTimer(self)
+        self.dance_timer.timeout.connect(self.dance_event)
+        self.dance_timer.start(100)
+
+        self.continue_timer = QTimer(self)
+        self.continue_timer.timeout.connect(self._continue_misha)
+        self.continue_timer.start(9500)
+        
+
     @thread
     def mouse(self):
         with Listener(on_move=self.on_move) as listener:
@@ -329,6 +426,15 @@ class Window(QMainWindow):
                 new_x = max(self.x_limit[0], min(self.current_x, self.x_limit[1]))
                 new_y = max(self.y_limit[0], min(self.current_y, self.y_limit[1]))
                 pyautogui.moveTo(new_x, new_y)
+
+    def dance_event(self):
+        if self.player.height() != self.misha_ult_2_skin.width():
+            self.player.resize(self.misha_ult_2_skin.width(),
+                               self.misha_ult_2_skin.height())
+        
+        self.dance.setStyleSheet(f"background-color: {self.colors[self.color_index].name()};")
+        self.color_index = (self.color_index + 1) % len(self.colors)
+            
 
     def _continue_sania(self):
         self.all_timer()
@@ -369,6 +475,23 @@ class Window(QMainWindow):
         else:
             self.player.setPixmap(self.stas_defoult_skin)
 
+    def _continue_misha(self):
+        self.all_timer()
+
+        self.continue_timer.stop()
+        self.dance_timer.stop()
+        
+        self.in_work = False
+        
+        self.dance.deleteLater()
+        
+        self.setWindowFlags(self.window_flags)
+        self.show()
+        
+        self.player.setPixmap(self.misha_defoult_skin)
+        self.player.resize(self.misha_defoult_skin.width(),
+                           self.misha_defoult_skin.height())
+
     def _chose_player(self, player_chose):
         self.player_chose = player_chose
         if self.player_chose == 'sania':
@@ -376,17 +499,31 @@ class Window(QMainWindow):
 
             self.player.resize(self.sania_defoult_skin.width(),
                                self.sania_defoult_skin.height())
-        else:
+        elif self.player_chose == 'stas':
             self.player.setPixmap(self.stas_defoult_skin)
 
             self.player.resize(self.stas_defoult_skin.width(),
                                self.stas_defoult_skin.height())
+        elif self.player_chose == 'misha':
+            self.player.setPixmap(self.misha_defoult_skin)
+
+            self.player.resize(self.misha_defoult_skin.width(),
+                               self.misha_defoult_skin.height())
+        
+        elif self.player_chose == 'kolia':
+            self.player.setPixmap(self.kolia_defoult_skin)
+
+            self.player.resize(self.kolia_defoult_skin.width(),
+                               self.kolia_defoult_skin.height())
+        
             
         self.animation = QPropertyAnimation(self.player, b"geometry")
         self.animation.setDuration(1000)
         
         self.sania_but.deleteLater()
         self.stas_but.deleteLater()
+        self.misha_but.deleteLater()
+        self.kolia_but.deleteLater()
 
         self.all_timer()
 
